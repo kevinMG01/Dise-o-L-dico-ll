3b1fab65f572a8169785786fce46be3bff2c3607
kevinMG01 <montanokevin46@gmail.com>
2023-03-24 15:52:33 -0300

    salto de player 1
    creamos la gravedad y el salto de player 1

--- Player_l/Player_l.gd
+++ Player_l/Player_l.gd
@@ -1,8 +1,23 @@
 extends KinematicBody2D
 
-
+#movimiento
 var cantidad = 100
 var movimiento = Vector2()
+#gravedad
+var gravedad = 20.0
+var masa = 1
+#salto
+const acceleracion = 70
+const max_speed = 320
+
+var speed = 10
+var jump_speed = 620
+var gravity = 22
+#max daltos
+var max_jump = 2
+var salto = 0
+var suelo = true
+var jump_doble = 720
 
 
 func _ready():
@@ -11,7 +26,9 @@ func _ready():
 
 func _physics_process(delta):
 	move()
-	movimiento = move_and_slide(movimiento)
+	jump()
+	movimiento.y = movimiento.y + gravedad
+	movimiento = move_and_slide(movimiento, Vector2(0, -1))
 	movimiento.x = lerp(movimiento.x,0,0.21)
 	pass
 
@@ -21,4 +38,24 @@ func move():
 		movimiento.x -= cantidad
 	if Input.is_action_pressed("ui_right"):
 		movimiento.x += cantidad
-	pass
+
+func jump():
+	if is_on_floor():
+		salto = 0
+		suelo = true
+		
+		if Input.is_action_pressed("ui_accept") && salto < max_jump:
+			print("hola")
+			suelo = false
+			if salto == 0:
+				#animacion de salto
+				pass
+			salto += 1
+			movimiento.y -= jump_speed
+		elif salto == 1:
+			#animacion de doble salto
+			movimiento.y -= jump_doble
+			salto +=1
+	if !is_on_floor():
+		if movimiento.y < 1:
+			pass
