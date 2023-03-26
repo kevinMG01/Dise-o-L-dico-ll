78b25ebd2aac5ee44d549f4fad805a2fda9678db
kevinMG01 <montanokevin46@gmail.com>
2023-03-26 15:39:34 -0300

    hgg
    iugh

--- Player_l/Player_l.gd
+++ Player_l/Player_l.gd
@@ -6,18 +6,16 @@ var movimiento = Vector2()
 #gravedad
 var gravedad = 20.0
 var masa = 1
-#salto
-const acceleracion = 70
-const max_speed = 320
 
+#salto
 var speed = 10
 var jump_speed = 620
-var gravity = 22
+
 #max daltos
 var max_jump = 2
 var salto = 0
 var suelo = true
-var jump_doble = 720
+
 
 
 func _ready():
@@ -46,15 +44,15 @@ func jump():
 		
 		if Input.is_action_just_pressed("ui_up") && salto < max_jump:
 			suelo = false
-			if salto == 0:
-				#animacion de salto
-				pass
+	#		if salto == 0:
+	#			#animacion de salto
+	#			pass
 			salto += 1
 			movimiento.y -= jump_speed
-		elif salto == 1:
+	#	elif salto == 1:
 			#animacion de doble salto
-			movimiento.y -= jump_doble
-			salto +=1
-	if !is_on_floor():
-		if movimiento.y < 1:
-			pass
+	#		movimiento.y -= jump_doble
+	#		salto +=1
+	#if !is_on_floor():
+	#	if movimiento.y < 1:
+	#		pass
