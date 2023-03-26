5ae43d96bcfa11e51c62e7de2a51859f2a9ee5c9
kevinMG01 <montanokevin46@gmail.com>
2023-03-26 17:06:36 -0300

    kan
    s

--- Mundo/Mundo.gd
+++ Mundo/Mundo.gd
@@ -22,11 +22,16 @@ func _physics_process(delta):
 	
 func _rotacion_player_1():
 	if global_Var.plat_player_1 == 1:
-		get_node("plataforma_player_l").rotation = normal
-		sumar_p_1 = 0
-	if global_Var.plat_player_1 == 2:
-		get_node("plataforma_player_l").rotation = speed
+		get_node("plata_player_l/plataforma_player_l").rotation = normal
 	
+		sumar_p_1 = 0
+		
+	if global_Var.plat_player_1 == 2:
+		get_node("plata_player_l/plataforma_player_l").rotation = speed
+	
+		
+		
+		
 #		rotacion de plataforma
 #	if global_Var.plat_player_1 == 1:
 #		get_node("plataforma_player_l").rotation = normal
@@ -38,10 +43,15 @@ func _rotacion_player_1():
 #			get_node("plataforma_player_l").rotation = rot_fija_1
 
 func _rotacion_player_2():
-	if global_Var.plat_player_2 == 1:
-		get_node("plataforma_player_ll").rotation = normal
-	if global_Var.plat_player_2 == 2:
-		get_node("plataforma_player_ll").rotate(speed)
-		sumar_p_2 += 0.03
-	if sumar_p_2 >= 9.10:
-		get_node("plataforma_player_ll").rotation = rot_fija_2
+	
+		if global_Var.plat_player_2 == 1:
+			get_node("plata_player_ll/plataforma_player_ll").rotation = normal
+		if global_Var.plat_player_2 == 2:
+			get_node("plata_player_ll/plataforma_player_ll").rotation = speed
+#	if global_Var.plat_player_2 == 1:
+#		get_node("plataforma_player_ll").rotation = normal
+#	if global_Var.plat_player_2 == 2:
+#		get_node("plataforma_player_ll").rotate(speed)
+#		sumar_p_2 += 0.03
+#	if sumar_p_2 >= 9.10:
+#		get_node("plataforma_player_ll").rotation = rot_fija_2
