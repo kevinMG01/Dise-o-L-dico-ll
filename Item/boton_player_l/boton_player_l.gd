157bee3cedcab00546ace2519950df8669d30115
kevinMG01 <montanokevin46@gmail.com>
2023-03-26 21:03:44 -0300

    no
    no habansamos

--- Item/boton_player_l/boton_player_l.gd
+++ Item/boton_player_l/boton_player_l.gd
@@ -13,5 +13,16 @@ func _on_boton_player_l_body_entered(body):
 func _on_boton_player_l_body_exited(body):
 	if body.is_in_group("player_l"):
 		global_Var.plat_player_1 = 1
-#		self.queue_free()
+#			$Area2D/CollisionShape2D.disabled = true
+		$CollisionShape2D.disabled = true
+		visible = false
+		$Timer.start()
+	if body.is_in_group("basura"):
+		queue_free()
+		pass
 	pass 
+
+
+func _on_Timer_timeout():
+	
+	pass # Replace with function body.
