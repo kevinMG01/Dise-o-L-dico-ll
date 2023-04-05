e649d4e1b25b21957fc58ede902b5c9b2db6d309
kevinMG01 <montanokevin46@gmail.com>
2023-04-04 23:35:21 -0300

    lava
    si

---
+++ Enemigo_sin_colicion/Enemigo.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+
+var player_1 = preload("res://Player_l/Player_l.tscn")
+
+
+func _ready():
+	pass 
+
+
+func _physics_process(delta):
+
+
+	print($Timer) 
+
+
+
+
+func _on_Enemigo_body_entered(body):
+	
+	pass # Replace with function body.
+
+
